49b78445a898830817c849d2ee9c4a96534e0577
Edy Segura <ednardo@inatel.br>
2016-11-30 23:14:49 -0200

    Added studentController.js

---
+++ public/js/studentController.js
@@ -0,0 +1,20 @@
+(function () {
+    "use strict"
+
+    function getStudentById(id) {
+        $.ajax({
+            method: "GET",
+            url: "/api/students/" + id
+        })
+        .done(student => {
+            console.log(student)
+        });
+    }
+
+    $("form").on("submit", event => {
+        let id = $("#studentId").val()
+        getStudentById(id)
+        return false
+    })
+    
+})()
