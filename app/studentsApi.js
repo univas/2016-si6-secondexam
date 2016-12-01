ff73583b98c7daa325e8de11f1f03f212675d0b6
Simão Faustino Laudino <info.simao@gmail.com>
2016-12-01 20:55:51 -0200

    studentAPI terminated

--- app/studentsApi.js
+++ app/studentsApi.js
@@ -4,6 +4,28 @@ const studentDB = require("./studentsDB.json")
 
 //api
 
+function catchNameStudent(studentId){
+    return studentsDB.find(student => student.id === studentId)
+}
+
+
+router.get("/", (request, response) => {
+    response.json(studentsDB)
+})
+
+router.get("/:studentId", (request, response) => {
+    let studentId = request.params.studentId
+    let studentData = catchNameStudent(studentId)
+    
+    if(studentData){
+        response.json(studentData)
+    }
+    else{
+        response.status(404).send("Not Found")
+    }
+})
+
+
 function situacao(nota1, nota2, nota3, nota4){
     var media = (nota1 + nota2 + nota3 + nota4) / 4
     var mensagem
@@ -19,3 +41,5 @@ function situacao(nota1, nota2, nota3, nota4){
     }
 
 }
+
+module.exports = router
