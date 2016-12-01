391e4d2abcdb7b494bc1ad3a7b40abe5a801fd67
Sabrina Andrade <saandradepereira@gmail.com>
2016-12-01 20:48:35 -0200

    "Students API"

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.get("/", function (request, response) {
   response.sendFile('index.html')
 })
 
+app.use("/api/students", require("./app/studentsApi"))
+
 const port = 9090
 const hostname = 'localhost'
 app.listen(port, _ => console.log(`Server started: http://${hostname}:${port}`))
