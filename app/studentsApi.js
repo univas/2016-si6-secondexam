6641e38d3c047db0f0133f4b3441b4ef582ef29d
Auro Mota <aurofmota@gmail.com>
2016-12-01 19:42:38 -0200

    Adding situation to student

--- app/studentsApi.js
+++ app/studentsApi.js
@@ -10,7 +10,8 @@ module.exports = (() => {
     function getStudent(req, res, next) {
         let id = req.query.mat;
         if (id) {
-            let student = students.find(student => student.id === id)
+            let student = students.find(student => student.id === id);
+            student.situation = getSituation(student);
             if (student) {
                 res.status(200).send(student);
             } else {
@@ -19,5 +20,23 @@ module.exports = (() => {
         } else {
             res.status(400).send({ message: 'Parameter \'mat\' is missing in the query string.' });
         }
+    }
+
+    function getSituation(student) {
+        let grade = calculateGrade(student);
+        if (grade < 50) {
+            return 'Disapproved';
+        }
+        if (grade < 60) {
+            return 'Final';
+        }
+        return 'Approved';
+    }
+
+    function calculateGrade(student) {
+        if (student.grades.length) {
+            return student.grades.reduce((previous, current) => previous + current, 0) / student.grades.length;
+        }
+        return 0;
     }
 })();
