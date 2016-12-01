874a0fea7aa94ecfd3b17fa6627c8b3817d97dfe
Douglas Oliveira <douglinhas_uchiha@hotmail.com>
2016-12-01 19:48:01 -0200

    addedBodyParser

--- public/server.js
+++ public/server.js
@@ -1,13 +1,17 @@
 "use strict"
 
+const bodyParser = require ('body-parser')
 const express = require ('express')
 const api = express()
 
 const port = 8080
 const hostname = 'localhost'
 
+app.use(bodyParser())
+app.use(express.static("public"))
+
 app.listen (port, onStart())
 
-function osStart() {
+function onStart() {
     console.log(`Server started at http://${hostname}:${port}`)
 }
