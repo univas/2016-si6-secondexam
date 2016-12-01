4782072d389d66f13f84f39a932c0400a9d81832
Gustavo Martins <gustavo_sh8@hotmail.com>
2016-12-01 20:06:02 -0200

    Added new route

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 const path = __dirname + "/public/";
 
-module.exports = function(app) {
+module.exports = function(app) {    
     app.get("/", function(request, response) {
         response.sendFile(path + "index.html");
+    });
+    
+    app.get("/api/students/:id", function (request, response) {
+        const id = request.params.id;
+        console.log(id);
     });
 }
