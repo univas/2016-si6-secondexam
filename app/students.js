aa1d810414334f7260b0b2f1af3f74e42d8e4d4d
Alison Rafael <armgalison@gmail.com>
2016-12-01 19:47:35 -0200

    created route to get a student

--- app/students.js
+++ app/students.js
@@ -1,10 +1,25 @@
 "use strict"
 const express = require('express')
 const router = express.Router()
-var studentsDB = require('studensDB.json')
+var studentsDB = require('./studentsDB.json')
 
-router.get('/', function (request, response) {
-    response.json({teste: "teste"})
-})
+function findStudent(studentId) {
+    return studentsDB.find(student => student.id == studentId)
+}
+
+router
+    .get('/', function (request, response) {
+        response.json(studentsDB)
+    })
+    .get('/:id', function (request, response) {
+        let requestId = request.params.id
+        let findedStudent = findStudent(requestId)
+        if(findedStudent){
+            response.json(findedStudent)
+        } else {
+            response.json({error: 'Student not found!'})
+        }
+    })
+
 
 module.exports = router
