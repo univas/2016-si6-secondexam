544ac391e231dd1e36fa9bc8e4329cadcd3909e7
Liipe <luiz-dasilva@hotmail.com>
2016-12-01 22:01:51 -0200

    Fixed index, server and router.js

--- app/studentApi.js
+++ app/studentApi.js
@@ -2,18 +2,19 @@
 
 // dependencies
 const express = require("express");
-const router = express.Router();
+var router = express.Router();
+var router = require('./router.js');
 const studentDB = require("./studentDB.json");
 
 // private
-function catchPokemon(studentId) {
+function checkStudents(studentId) {
     return studentDB.find(student => studentId.id === studentId);
 }
 
 //Função de verificação do estudante
 
 //Função para pegar o ID do estudante
-function catchStudent(request, response) {
+function checkStudents(request, response) {
     var studentID = request.query.mat;
     if (studentID) {
         
@@ -38,7 +39,7 @@ function checkSituation() {
     if (note < 50) {
         return 'REPROVADO';
     }
-    else if (note >=50 && < 60) {
+    else if (note >=50 && <60) {
         return 'FINAL';
     }
     else {
@@ -53,12 +54,12 @@ router.get("/", (request, response) => {
 
 router.get("/:studentId", (request, response) => {
    var studenId = request.params.studentId
-   var studentData = catchPokemon(studentId);
+   var studentData = checkStudents(studentId);
    if (studentData) {
       response.json(studentData);
    }
    else {
-      response.status(404).send("Not Found!")
+      response.status(404).send("Error 404 - Not Found!")
    }
 });
 
--- public/router.js
+++ public/router.js
@@ -14,4 +14,5 @@ module.exports = function(app) {
         const id = request.params.id;
        console.log(id);
     });
-} 
+};
+
